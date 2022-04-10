1c59a2488e508f002188011d70e4d4f0cbf9ee6e
galihif <galihindra2401@mail.ugm.ac.id>
2022-04-10 13:47:03 +0700

    style: add action bar in HomeActivity.kt

--- app/src/main/java/com/giftech/instagramclone/ui/home/HomeActivity.kt
+++ app/src/main/java/com/giftech/instagramclone/ui/home/HomeActivity.kt
@@ -1,8 +1,13 @@
 package com.giftech.instagramclone.ui.home
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.giftech.instagramclone.R
 import com.giftech.instagramclone.core.utils.AppUtils
 import com.giftech.instagramclone.core.viewmodel.ViewModelFactory
 import com.giftech.instagramclone.databinding.ActivityHomeBinding
@@ -15,10 +20,18 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        showActionBar()
         setupViewModel()
 
         getUserData()
+    }
+
+    private fun showActionBar() {
+        supportActionBar?.title = ""
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        val inflater: LayoutInflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.action_bar, null)
+        supportActionBar?.customView = view
     }
 
     private fun getUserData() {
@@ -29,5 +42,19 @@ class HomeActivity : AppCompatActivity() {
     private fun setupViewModel() {
         val factory = ViewModelFactory.getInstance(this)
         viewModel = ViewModelProvider(this,factory)[HomeViewModel::class.java]
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item_add -> AppUtils.showToast(this, "Add")
+            R.id.item_logout -> AppUtils.showToast(this, "Logout")
+        }
+        return true
     }
 }
