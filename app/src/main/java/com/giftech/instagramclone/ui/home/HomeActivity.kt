0cc6b63f5e17bb1f3ce8f9a1164c2474123dad7d
galihif <galihindra2401@mail.ugm.ac.id>
2022-04-10 11:09:12 +0700

    feat: get data from pref and show in toast

--- app/src/main/java/com/giftech/instagramclone/ui/home/HomeActivity.kt
+++ app/src/main/java/com/giftech/instagramclone/ui/home/HomeActivity.kt
@@ -2,11 +2,32 @@ package com.giftech.instagramclone.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.giftech.instagramclone.R
+import androidx.lifecycle.ViewModelProvider
+import com.giftech.instagramclone.core.utils.AppUtils
+import com.giftech.instagramclone.core.viewmodel.ViewModelFactory
+import com.giftech.instagramclone.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var viewModel: HomeViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViewModel()
+
+        getUserData()
+    }
+
+    private fun getUserData() {
+        val user = viewModel.getUser()
+        AppUtils.showToast(this, user.username)
+    }
+
+    private fun setupViewModel() {
+        val factory = ViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(this,factory)[HomeViewModel::class.java]
     }
 }
