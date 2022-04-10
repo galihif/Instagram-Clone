fc1b1df966079558d205d6fe23537f54a41577fc
galihif <galihindra2401@mail.ugm.ac.id>
2022-04-10 17:46:34 +0700

    feat: use cameraX to take photo

--- app/src/main/java/com/giftech/instagramclone/ui/post/selectphoto/SelectPhotoActivity.kt
+++ app/src/main/java/com/giftech/instagramclone/ui/post/selectphoto/SelectPhotoActivity.kt
@@ -1,16 +1,32 @@
 package com.giftech.instagramclone.ui.post.selectphoto
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.giftech.instagramclone.core.utils.AppUtils.rotateBitmap
 import com.giftech.instagramclone.core.utils.AppUtils.uriToFile
 import com.giftech.instagramclone.core.viewmodel.ViewModelFactory
 import com.giftech.instagramclone.databinding.ActivitySelectPhotoBinding
+import com.giftech.instagramclone.ui.post.camera.CameraActivity
+import java.io.File
 
 class SelectPhotoActivity : AppCompatActivity() {
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
 
     private lateinit var binding: ActivitySelectPhotoBinding
     private lateinit var viewModel: SelectPhotoViewModel
@@ -23,14 +39,30 @@ class SelectPhotoActivity : AppCompatActivity() {
 
         setupViewModel()
 
+        requestPermission()
+
         binding.containerHeader.btnClose.setOnClickListener {
             onBackPressed()
+        }
+
+        binding.btnCamera.setOnClickListener {
+            openCameraX()
         }
 
         binding.btnGallery.setOnClickListener {
             openGallery()
         }
 
+    }
+
+    private fun requestPermission() {
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
     }
 
     private fun openGallery() {
@@ -49,6 +81,49 @@ class SelectPhotoActivity : AppCompatActivity() {
             val myFile = uriToFile(selectedImg, this)
             binding.ivImage.setImageURI(selectedImg)
         }
+    }
+
+    private fun openCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+
+            binding.ivImage.setImageBitmap(result)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun hideActionBar() {
