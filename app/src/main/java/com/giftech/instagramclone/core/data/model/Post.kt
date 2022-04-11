b30ed8be5dbc6204bab32b33d1eccd3675103b45
galihif <galihindra2401@mail.ugm.ac.id>
2022-04-11 09:18:47 +0700

    feat: add post data class

---
+++ app/src/main/java/com/giftech/instagramclone/core/data/model/Post.kt
@@ -0,0 +1,8 @@
+package com.giftech.instagramclone.core.data.model
+
+data class Post(
+    var id:String = "",
+    var username:String = "",
+    var caption:String = "",
+    var photo:String = ""
+)
