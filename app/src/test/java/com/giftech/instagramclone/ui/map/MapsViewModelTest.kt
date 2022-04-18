dc1599ee1826acc4241fad98ccd17f982668c137
galihif <galihindra2401@mail.ugm.ac.id>
2022-04-18 11:45:57 +0700

    test: add test get post with location

---
+++ app/src/test/java/com/giftech/instagramclone/ui/map/MapsViewModelTest.kt
@@ -0,0 +1,46 @@
+package com.giftech.instagramclone.ui.map
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.giftech.instagramclone.DataDummy
+import com.giftech.instagramclone.core.data.MainRepository
+import com.giftech.instagramclone.core.data.model.Post
+import com.giftech.instagramclone.getOrAwaitValue
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MapsViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var mainRepository: MainRepository
+    private lateinit var mapsViewModel: MapsViewModel
+    private val dummyPost = DataDummy.generateDummyListPostLocation()
+
+    @Before
+    fun setUp() {
+        mapsViewModel = MapsViewModel(mainRepository)
+    }
+
+    @Test
+    fun `when Get Post With Location Should Not Null and Return Same Result`(){
+        val expectedPost = MutableLiveData<List<Post>>()
+        expectedPost.value = dummyPost
+        `when`(mapsViewModel.getPostWithLocation()).thenReturn(expectedPost)
+
+        val actualPost = mapsViewModel.getPostWithLocation().getOrAwaitValue()
+        Mockito.verify(mainRepository).getPostWithLocation()
+        Assert.assertNotNull(actualPost)
+        Assert.assertEquals(dummyPost.size, actualPost.size)
+    }
+}
