90e0838d93f53fb17cf4579b22ae42a94370b16f
galihif <galihindra2401@mail.ugm.ac.id>
2022-04-18 20:56:17 +0700

    test: create LoginViewModelTest.kt

---
+++ app/src/test/java/com/giftech/instagramclone/ui/auth/login/LoginViewModelTest.kt
@@ -0,0 +1,54 @@
+package com.giftech.instagramclone.ui.auth.login
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.giftech.instagramclone.DataDummy
+import com.giftech.instagramclone.MainCoroutineRule
+import com.giftech.instagramclone.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class LoginViewModelTest{
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+    @get:Rule
+    var mainCoroutineRules = MainCoroutineRule()
+    @Mock
+    private lateinit var viewModel: LoginViewModel
+
+    @Test
+    fun `when login should return true and not null`() = mainCoroutineRules.runBlockingTest {
+        val dummyResult = MutableLiveData<Boolean>(true)
+        val dummyUser = DataDummy.generateDummyUser()
+        Mockito.`when`(viewModel.login(dummyUser)).thenReturn(dummyResult)
+
+        val actualResult = viewModel.login(dummyUser).getOrAwaitValue()
+        Mockito.verify(viewModel).login(dummyUser)
+        Assert.assertNotNull(actualResult)
+        Assert.assertEquals(dummyResult.value, actualResult)
+    }
+
+    @Test
+    fun `when login should return false and not null`() = mainCoroutineRules.runBlockingTest {
+        val dummyResult = MutableLiveData<Boolean>(false)
+        val dummyUser = DataDummy.generateDummyUser()
+        Mockito.`when`(viewModel.login(dummyUser)).thenReturn(dummyResult)
+
+        val actualResult = viewModel.login(dummyUser).getOrAwaitValue()
+        Mockito.verify(viewModel).login(dummyUser)
+        Assert.assertNotNull(actualResult)
+        Assert.assertEquals(dummyResult.value, actualResult)
+    }
+
+
+}
